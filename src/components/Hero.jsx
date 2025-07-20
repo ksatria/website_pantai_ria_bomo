e1514dd27bbfd74d3ed24b8346b2f4f90d9c69f4
putrafir <firdausputra0212@gmail.com>
2025-07-20 18:46:38 +0700

    fix image hero slider

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,22 +9,29 @@ const images = [
 
 export default function Hero() {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [showContent, setShowContent] = useState(true);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) =>
-        prevIndex === images.length - 1 ? 0 : prevIndex + 1
-      );
+      setShowContent(false);
+      setTimeout(() => {
+        setCurrentIndex((prevIndex) =>
+          prevIndex === images.length - 1 ? 0 : prevIndex + 1
+        );
+
+        setTimeout(() => {
+          setShowContent(true);
+        }, 1000);
+      }, 500);
     }, 4000);
     return () => clearInterval(interval);
-  }, []);
+  }, [currentIndex]);
 
   return (
     <section
       id="home"
       className="relative overflow-hidden min-h-[600px] md:h-screen flex items-center justify-center"
     >
-      {/* Background Images */}
       <div
         className="absolute inset-0 flex transition-transform duration-1000 ease-in-out"
         style={{ transform: `translateX(-${currentIndex * 100}%)` }}
@@ -39,8 +46,11 @@ export default function Hero() {
         ))}
       </div>
 
-      {/* Overlay Content */}
-      <div className="relative z-10 px-4 mx-auto max-w-screen-xl text-center text-white py-24 lg:py-56">
+      <div
+        className={`relative z-10 px-4 mx-auto max-w-screen-xl text-center text-white py-24 lg:py-56 transition-all duration-500 ${
+          showContent ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
+        }`}
+      >
         <h1 className="mb-4 text-xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl">
           Temukan Kembali <br /> Ketenangan Anda di Sini
         </h1>
@@ -51,7 +61,7 @@ export default function Hero() {
         <div className="flex flex-col items-center space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
           <a
             href="#"
-            className="w-fit inline-flex justify-center items-center px-3 text-xs py-3 sm:px-5 sm:text-base font-medium text-center text-white rounded-lg bg-[#D35F1C] hover:bg-[#BC4F10]"
+            className="w-fit inline-flex justify-center items-center px-3 text-xs py-3 sm:px-5 sm:text-base font-medium text-center text-white rounded-lg bg-[#D35F1C] hover:bg-[#BC4F10] transition-all duration-500"
           >
             Temukan Kedamaian Anda
           </a>
