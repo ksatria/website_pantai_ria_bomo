cb9ba6709f464ce114530668f87c9741c8ceab51
putrafir <firdausputra0212@gmail.com>
2025-08-08 20:29:51 +0700

    fix: a image layanan

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,19 +1,15 @@
 export default function CardItem({ image, title, desc }) {
   return (
     <div className="max-w-sm h-full md:min-h-[410px] bg-white border border-gray-200 rounded-lg shadow-sm ">
-      <a href="#">
-        <img
-          className="rounded-t-lg w-full h-full object-cover"
-          src={image}
-          alt=""
-        />
-      </a>
+      <img
+        className="rounded-t-lg w-full h-full object-cover"
+        src={image}
+        alt=""
+      />
       <div className="p-5">
-        <a href="#">
-          <h5 className="mb-2 text-2xl md:text-lg xl:text-2xl font-bold tracking-tight text-gray-900 ">
-            {title}
-          </h5>
-        </a>
+        <h5 className="mb-2 text-2xl md:text-lg xl:text-2xl font-bold tracking-tight text-gray-900 ">
+          {title}
+        </h5>
         <p className="mb-3 font-normal text-base md:text-sm text-gray-700 ">
           {desc}
         </p>
