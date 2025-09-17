7aedc5292cf37981d8191f8948e72cc198679d74
Khoirul Umam <khoirul.umam35@gmail.com>
2025-09-17 10:29:23 +0700

    Update pengurus_translations.js
    Nama organisasi tidak boleh ditranslate ke bahasa asing, kecuali organisasi tersebut sudah punya nama resmi dalam bahasa asing

--- src/utils/pengurus_translations.js
+++ src/utils/pengurus_translations.js
@@ -1,8 +1,8 @@
 export const pengurus_translations = {
-  id: {
-    title: "Struktur Organisasi <br> Benteng Samudra Desa Bomo",
-  },
-  en: {
-    title: "Organizational structure <br>of the ocean fortress of Bomo village",
-  },
+	id: {
+		title: 'Struktur Organisasi <br> Benteng Samudra Desa Bomo',
+	},
+	en: {
+		title: 'Organizational structure <br>Benteng Samudra Desa Bomo',
+	},
 };
